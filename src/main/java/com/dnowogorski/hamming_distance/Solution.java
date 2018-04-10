c8ba42e9490279daa7c927ab7c2cdfcfe80a3365
dnowogorski <reponagicie.x01>
2018-04-10 21:11:14 +0200

    Setup next problem

---
+++ src/main/java/com/dnowogorski/hamming_distance/Solution.java
@@ -0,0 +1,7 @@
+package com.dnowogorski.hamming_distance;
+
+public class Solution {
+    public int hammingDistance(int x, int y) {
+        return 0;
+    }
+}
---
+++ src/test/java/com/dnowogorski/hamming_distance/SolutionSpec.groovy
@@ -0,0 +1,4 @@
+package com.dnowogorski.hamming_distance
+
+class SolutionSpec {
+}
