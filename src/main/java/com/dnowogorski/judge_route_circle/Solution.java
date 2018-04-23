85927b69752dd0c9bdcf502d6b25e8a669891846
dnowogorski <reponagicie.x01>
2018-04-23 20:18:44 +0200

    Use switch instead of ifs

--- src/main/java/com/dnowogorski/judge_route_circle/Solution.java
+++ src/main/java/com/dnowogorski/judge_route_circle/Solution.java
@@ -8,10 +8,12 @@ class Solution {
         int x = 0;
         int y = 0;
         for (char c: moves.toCharArray()) {
-            if (c == 'U') y++;
-            if (c == 'D') y--;
-            if (c == 'L') x--;
-            if (c == 'R') x++;
+            switch (c) {
+                case 'U' : y++; break;
+                case 'D' : y--; break;
+                case 'L' : x--; break;
+                case 'R' : x++; break;
+            }
         }
         return x == 0 && y == 0;
     }
