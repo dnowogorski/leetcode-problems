a7b745752c973f72807f773d442b54a9c20ef8cd
dnowogorski <reponagicie.x01>
2018-06-04 20:28:30 +0200

    Add solution and tests

--- src/main/java/com/dnowogorski/array_partition/Solution.java
+++ src/main/java/com/dnowogorski/array_partition/Solution.java
@@ -1,7 +1,14 @@
 package com.dnowogorski.array_partition;
 
+import java.util.Arrays;
+
 public class Solution {
     public int arrayPairSum(int[] nums) {
-        return 0;
+        Arrays.sort(nums);
+        int sum = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            sum += Math.min(nums[i], nums[i + 1]);
+        }
+        return sum;
     }
 }
--- src/test/java/com/dnowogorski/array_partition/SolutionSpec.groovy
+++ src/test/java/com/dnowogorski/array_partition/SolutionSpec.groovy
@@ -1,5 +1,18 @@
 package com.dnowogorski.array_partition
 
-class SolutionSpec {
+import spock.lang.Specification
+import spock.lang.Unroll
 
+class SolutionSpec extends Specification {
+    Solution solution = new Solution()
+
+    @Unroll
+    def "should find max sum for pairs"() {
+        expect:
+        solution.arrayPairSum(nums as int[]) == result
+
+        where:
+        nums << [[1, 4, 3, 2], [1, 1]]
+        result << [4, 1]
+    }
 }
