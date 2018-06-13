bc3e8e2cfadff7fb2421b951f4ef68a5cef32ffe
dnowogorski <nowogorski.dawid@gmail.com>
2018-06-13 19:07:12 +0200

    Add final solution

--- src/main/java/com/dnowogorski/reverse_words_in_string_3/Solution.java
+++ src/main/java/com/dnowogorski/reverse_words_in_string_3/Solution.java
@@ -1,7 +1,21 @@
 package com.dnowogorski.reverse_words_in_string_3;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Solution {
+
+    private static final String SPACE = " ";
+
     public String reverseWords(String s) {
-        return null;
+        return Arrays.stream(s.split(SPACE))
+                .map(this::reverse)
+                .collect(Collectors.joining(SPACE));
+    }
+
+    private String reverse(String word) {
+        return new StringBuilder(word)
+                .reverse()
+                .toString();
     }
 }
