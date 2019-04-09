acfeb330c404f5dd6c53138cd6fb0ac7251144d7
dnowogorski <nowogorski.dawid@gmail.com>
2019-04-09 17:24:24 +0200

    Initial solution

--- src/main/java/com/dnowogorski/outer_parentheses/Solution.java
+++ src/main/java/com/dnowogorski/outer_parentheses/Solution.java
@@ -1,7 +1,25 @@
 package com.dnowogorski.outer_parentheses;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Solution {
     public String removeOuterParentheses(String input) {
-        return "result";
+        Deque<Character> stack = new ArrayDeque<>();
+        StringBuilder result = new StringBuilder();
+        int beginIndex = 0;
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            if (ch == '(') {
+                stack.push(ch);
+            } else {
+                stack.pop();
+                if (stack.isEmpty()) {
+                    result.append(input, beginIndex + 1, i);
+                    beginIndex = i + 1;
+                }
+            }
+        }
+        return result.toString();
     }
 }
